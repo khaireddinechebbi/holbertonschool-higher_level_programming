ce1258324f1b7728e044c5dd3b031946df50832a
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-09 15:52:57 +0100

    print

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    n = abs(number) % 10
-    if number < 0:
-        n = n * -1
-    return n
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
