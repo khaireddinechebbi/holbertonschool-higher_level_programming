e67a6c6e9a069d65021eb05b7f8cb8347a7c883b
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-09 15:19:37 +0100

    upp

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ''
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end='')
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print ("{}".format(result), end='')
     print()
