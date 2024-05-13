4f32e70ccdc3ba6b5d708002134534a698a33663
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-13 16:05:44 +0100

    if not matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, num in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(num))
-            else:
-                print("{:d}".format(num), end=" ")
     if not matrix:
-        print("\n")
+        print()
+    else:
+        for row in matrix:
+            for i, num in enumerate(row):
+                if i == len(row) - 1:
+                    print("{:d}".format(num))
+                else:
+                    print("{:d}".format(num), end=" ")
