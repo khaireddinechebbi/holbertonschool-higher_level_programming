879cda900ffc8dfefaf9eb679cc738a78cd86902
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-13 13:01:02 +0100

    element at list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     n = len(my_list)
-    if idx < 0:
-        return None
-    elif idx >= n:
+    if idx < 0 or idx >= n:
         return None
     else:
-        for i in my_list:
-            for j in range[0, n]:
-                if j == idx:
-                    print("{:d}".format(i))
+        print("{:d}".format(my_list[idx]))
