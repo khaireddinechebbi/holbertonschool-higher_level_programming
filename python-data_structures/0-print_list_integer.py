8ccd4b0ecb61386fc4a9fec39c93eb3f6c769c8d
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-13 12:49:01 +0100

    str.format

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     a = len(my_list)
     for i in range[0, a]:
-        result = "{i}\n".format(my_list)
+        result = str.format(my_list)
     print(result)
