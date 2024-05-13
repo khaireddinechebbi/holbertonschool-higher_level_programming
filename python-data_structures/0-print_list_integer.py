fa005367c1f8435e3c9122c5e3aa048ace468761
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-13 12:51:20 +0100

    print {:d} correction

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    a = len(my_list)
-    for i in range[0, a]:
-        result = str.format(my_list)
-    print(result)
+    for i in my_list:
+        print("{:d}".format(i))
