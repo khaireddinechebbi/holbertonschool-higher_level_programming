833570bb06d80fc63a7ba2b88930e4251c2c0b8c
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-14 11:10:12 +0100

    del my list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,8 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        result = []
-        for i in range(len(my_list)):
-            if i != idx:
-                result.append(my_list[i])
-        return result
+        del my_list[idx]
+        return my_list
