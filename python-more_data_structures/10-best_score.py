7c032354a036a7e6e272065ab99ee8f77f9bb4e2
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-14 22:19:24 +0100

    best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_key = None
+    best_score = 0
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best_score = value
+            best_key = key
+    return best_key
