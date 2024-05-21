5e304f2cc367f515cfc6179deed31cc7b01dd23e
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-22 00:01:14 +0100

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,7 @@ that prints a text with 2 new lines after each of \
 these characters: ., ? and :
 """
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
@@ -29,6 +30,7 @@ def text_indentation(text):
         else:
             print(text[i], end="")
             i += 1
+
 
 if __name__ == "__main__":
     import doctest
