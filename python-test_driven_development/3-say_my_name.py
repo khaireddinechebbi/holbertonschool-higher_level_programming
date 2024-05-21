934118ed921bff922a4cf392e1c6e935c6d7cdd1
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-21 13:46:50 +0100

    say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module provides a function `say_my_name` that prints a name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Prints "My name is <first_name> <last_name>".
+
+    Args:
+        first_name (str): The first name to be printed.
+        last_name (str): The last name to be printed (optional).
+
+    Raises:
+        TypeError: If `first_name` or `last_name` is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}".strip())
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/3-say_my_name.txt")
