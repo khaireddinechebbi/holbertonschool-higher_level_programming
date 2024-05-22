ea6f100e2608095c928824a9437886cc1316be5c
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-22 11:23:01 +0100

    module and class documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module define an empty class regtangle
+"""
 class Rectangle:
-    def __init__(self):
+        """
+        This is an empty class that defines a rectangle
+        """
         pass
