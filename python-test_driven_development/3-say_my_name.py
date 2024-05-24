70d0922cdaa65eea53f470e31d7cc4d6d1a6f920
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-24 12:05:37 +0100

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,10 +19,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name} ")
+    print(f"My name is {first_name} {last_name}")
 
 
 if __name__ == "__main__":
