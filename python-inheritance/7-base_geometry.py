b37e311b54c2d561584c5c21808ebd686a0d03e1
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-25 23:45:12 +0100

    base geo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,59 +1,17 @@
 #!/usr/bin/python3
-"""
-This module provides a class `BaseGeometry` for representing geometry.
-"""
+"""Write an empty geometry class."""
 
 
 class BaseGeometry:
-    """
-    A class used to represent geometry.
-
-    Attributes:
-        None
-
-    Methods:
-        area(self):
-            Raises an Exception with the message "area() is not implemented".
-        integer_validator(self, name, value):
-            Validates if the provided value is an integer greater than 0.
-    """
+    """Empty geometry class."""
 
     def area(self):
-        """
-        Raises an Exception with the message "area() is not implemented".
-
-        Args:
-            None
-
-        Returns:
-            None
-
-        Raises:
-            Exception: When called.
-        """
+        """Raise an exception."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates if the provided value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Returns:
-            None
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        """Validate value."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/7-base_geometry.txt")
+            raise ValueError("{} must be greater than 0".format(name))
