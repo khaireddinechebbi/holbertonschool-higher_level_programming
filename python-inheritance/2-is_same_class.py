add8c478de053d80a1f67b805aa82e709f74d111
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-25 22:35:22 +0100

    is a class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -16,4 +16,4 @@ def is_same_class(obj, a_class):
     Returns:
         bool: True if obj is exactly an instance of a_class, otherwise False.
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
