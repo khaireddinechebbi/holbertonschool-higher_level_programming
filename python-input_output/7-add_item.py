40cd7fbf72d4ea169bd9ddf98c596bd27dd02822
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-28 11:35:25 +0100

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script adds all command-line arguments to a Python list \
+This module adds all command-line arguments to a Python list \
 and saves them to a file.
 
 The list is saved as a JSON representation in a file named 'add_item.json'.
