7cf6d698ee20f4476d822a1cc126c98ec8477d83
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-29 09:17:54 +0100

    pep8 corrections

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -54,5 +54,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: self.__dict__[attr] for attr in attrs \
+            return {attr: self.__dict__[attr] for attr in attrs
                     if attr in self.__dict__}
