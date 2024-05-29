0b841c3624011a99b61de1678fc080c53a9bbb61
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-05-29 09:14:17 +0100

    attrs

---
+++ python-input_output/10-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Module defining the Student class.
+"""
+
+
+class Student:
+    """
+    A class to represent a student.
+
+    Attributes:
+    ----------
+    first_name : str
+        the first name of the student
+    last_name : str
+        the last name of the student
+    age : int
+        the age of the student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the Student instance with first name, last name, and age.
+
+        Parameters:
+        ----------
+        first_name : str
+            The first name of the student
+        last_name : str
+            The last name of the student
+        age : int
+            The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+        -------
+        dict
+            A dictionary representation of the student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            for attr in attrs:
+                if attr in self.__dict__:
+                    return {attr : self.__dict__[attr]}
+
