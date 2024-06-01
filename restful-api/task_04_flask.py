fbe329387f757a4531532ed7c677fd02495e9f99
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 23:17:49 +0100

    if not user data

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -47,10 +47,15 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     user_data = request.get_json()
-    username = user_data.get("username")
+    if not user_data:
+        return jsonify({"error": "Invalid input"}), 400
 
-    if not username or username in users:
-        return jsonify({"error": "Invalid or duplicate username"}), 400
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    if username in users:
+        return jsonify({"error": "Duplicate username"}), 400
 
     users[username] = user_data
     return jsonify({"message": "User added", "user": user_data}), 201
