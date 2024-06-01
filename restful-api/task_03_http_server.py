50c5feed864b0e5cb84e0ffdf7cf57e96d841af7
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 21:45:59 +0100

    task 3 status

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -17,28 +17,27 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         """
         Handle GET requests to serve different endpoints with appropriate responses.
         """
-        if self.path == "/":
+        if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        elif self.path == "/data":
+        elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            dataset = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(dataset).encode('utf-8'))
-        elif self.path == "/status":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            status = {"status": "OK"}
-            self.wfile.write(json.dumps(status).encode('utf-8'))
+            self.wfile.write(b"OK")
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 Not Found")
 
 
 if __name__ == "__main__":
