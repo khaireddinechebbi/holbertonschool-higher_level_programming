f0819858f20fa25301bfca87f4f38e4f570883dc
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 21:23:18 +0100

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -17,17 +17,25 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         """
         if self.path == "/":
             self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "\\data":
             self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
             dataset = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(dataset))
         elif self.path == "\\status":
             self.send_response(200)
-            return "ok"
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            return "404 Not Found"
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
 
 if __name__ == "__main__":
