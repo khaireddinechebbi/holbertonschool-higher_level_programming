ea0653101666aa56366fef6540d37ac2a4ce9a8b
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 23:10:41 +0100

    task 4

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+A simple Flask API for demonstration purposes.
+"""
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {
+    "jane": {
+        "username": "jane",
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"},
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"}}
+
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data')
+def get_usernames():
+    return jsonify(list(users.keys()))
+
+
+@app.route('/status')
+def status():
+    return "OK"
+
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    user_data = request.get_json()
+    username = user_data.get("username")
+
+    if not username or username in users:
+        return jsonify({"error": "Invalid or duplicate username"}), 400
+
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
+
+
+if __name__ == "__main__":
+    app.run()
