9b2e10e3d0a17b9023e6e3fd4e9a6f4707804250
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 20:15:41 +0100

    if post == title

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -16,7 +16,8 @@ def fetch_and_print_posts():
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post['title'])
+            if post == 'title':
+                print(post['title'])
     else:
         print("Failed to fetch posts.")
 
