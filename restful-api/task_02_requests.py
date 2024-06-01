dfe78b6ae67b118572b97c59fa4b455ed7f3fd54
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-01 22:06:56 +0100

    2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -11,7 +11,7 @@ def fetch_and_print_posts():
     Fetches posts from JSONPlaceholder and prints their titles.
     """
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"status code: {response.status_code}")
+    print("Status Code:", response.status_code)
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
