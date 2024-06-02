fc58b54d3669741e892ed07531265b3de15e4876
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-02 04:02:48 +0100

    task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,41 +4,26 @@ app = Flask(__name__)
 
 # Dictionary to store user data
 users = {
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"},
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"}}
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
 
 # Route to welcome message
-
-
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
 
 # Route to return list of usernames
-
-
 @app.route('/data')
 def data():
     return jsonify(list(users.keys()))
 
 # Route to return status
-
-
 @app.route('/status')
 def status():
     return "OK"
 
 # Route to return user data based on username
-
-
 @app.route('/users/<username>')
 def user(username):
     if username in users:
@@ -47,8 +32,6 @@ def user(username):
         return jsonify({"error": "User not found"})
 
 # Route to add new user
-
-
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
@@ -58,7 +41,6 @@ def add_user():
         return jsonify({"message": "User added", "user": data})
     else:
         return jsonify({"error": "Username not provided"}), 400
-
 
 if __name__ == '__main__':
     app.run(debug=True)
