35a3fd635dc2ef017eced3d480e351a29d864ba2
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-23 13:28:55 +0100

    cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- A script that creates the database hbtn_0d_usa and the table cities
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT FOREIGN KEY (states_id) REFERENCES states(id),
+    states_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+);
