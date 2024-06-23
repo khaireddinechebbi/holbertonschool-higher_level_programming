7403183c5fc7f073a5f25e6a60356776879e995d
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-23 13:05:15 +0100

    create user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+-- A script that creates the MySQL server user user_0d_1
+
+-- Create the user if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
