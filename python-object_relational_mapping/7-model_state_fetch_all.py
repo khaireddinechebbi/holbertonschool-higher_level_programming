23cb8435ba7f6c48b8449e6d6993970ad5102836
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-25 12:35:38 +0100

    pep8 corrections

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     states = session.query(State).order_by(State.id).all()
     for state in states:
         print(f"{state.id}: {state.name}")
-    
+
     session.close()
