daecd52daa74098577b1a3aba8787b5b37890fde
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-25 09:17:57 +0100

    filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '' ORDER BY id ASC"\
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
         .format(state_name_searched)
     cur.execute(query)
 
