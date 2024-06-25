dd6f57972986a554edb6cb681ea3650d1d0aed65
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-25 18:02:12 +0100

    new state

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = "Louisiana"
+    new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
 
