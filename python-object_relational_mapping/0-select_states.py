857caee5e37718e5f46e94ba90e3c78c11b7c5c0
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-06-25 08:01:27 +0100

    select, pep8 corrections

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,12 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database_name
+                         )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
