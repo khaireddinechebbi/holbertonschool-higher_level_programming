ef0c1f3690278be121a2f31eba0177ab95cb1409
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-07-04 23:34:54 +0100

    update

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,8 @@
 -- A script that lists all comedy shows
 
 SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+FROM tv_shows_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
